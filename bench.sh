dfa46cd286091c4e54736b41d0bb9fd7cdc680ee
Peter Portante <peter.portante@redhat.com>
2012-09-08 21:32:01 -0400

    A more header information, boot command line, VM settings, transparent huge page stuff, and also disable a few more behaviors, reenabling them whenever the script exists.
    We disable transparent huge pages because there is lots of automatic
    adjustments in the Linux kernel to such pages.
    
    We disable writebacks of data and dirty pages (well, delay it until the end of
    the benchmark) to avoid time taken away from the benchmark for that.

--- bench.sh
+++ bench.sh
@@ -64,7 +64,7 @@ while getopts "s:C:D:X:" optname; do
 done
 
 if [ $OPTIND -gt 1 ]; then
-    shift $((OPTIND - 1))
+	shift $((OPTIND - 1))
 fi
 
 if [ ! -e "$DIRNAME/suites/$suite.txt" ]; then
@@ -78,7 +78,6 @@ TMPROOT=/tmp/libmicro.$$
 VARROOT=/var/tmp/libmicro.$$
 mkdir -p $TMPROOT
 mkdir -p $VARROOT
-trap "rm -rf $TMPROOT $VARROOT" 0 2
 
 TFILE=$TMPROOT/data
 IFILE=$TMPROOT/ifile
@@ -87,6 +86,37 @@ TDIR2=$TMPROOT/1/2/3/4/5/6/7/8/9/0
 VFILE=$VARROOT/data
 VDIR1=$VARROOT/0/1/2/3/4/5/6/7/8/9
 VDIR2=$VARROOT/1/2/3/4/5/6/7/8/9/0
+
+function on_exit() {
+	rm -rf $TMPROOT $VARROOT
+
+	if [ "$UID" -eq "$ROOT_UID" ]
+	then
+		# Clean up background processes
+		if [ -n "$PM_QOS_PID" ]; then
+			kill -INT $PM_QOS_PID
+		fi
+
+		# Restore previous setting
+		if [ ! -z "$saved_thp_enable" ]; then
+			echo $saved_thp_enable > $THP_ENABLED
+		fi
+		if [ -n "$saved_dwc_value" ]; then
+			echo $saved_dwc_value > /proc/sys/vm/dirty_writeback_centisecs
+		fi
+		if [ -n "$saved_dec_value" ]; then
+			echo $saved_dec_value > /proc/sys/vm/dirty_expire_centisecs
+		fi
+		if [ -n "$saved_dirty_bg_ratio" ]; then
+			echo $saved_dirty_bg_ratio > /proc/sys/vm/dirty_background_ratio
+		fi
+		if [ -n "$saved_dirty_ratio" ]; then
+			echo $saved_dirty_ratio > /proc/sys/vm/dirty_ratio
+		fi
+	fi
+}
+
+trap on_exit 0 2
 
 dd if=/dev/zero of=$TFILE bs=1M count=1K 2>/dev/null
 dd if=/dev/zero of=$VFILE bs=1M count=1K 2>/dev/null
@@ -112,7 +142,26 @@ then
 	# Make an attempt to keep the system from going into
 	# energy savings mode (Intel systems only?).
 	$VARROOT/bin/pm_qos > /dev/null 2>&1 < /dev/null &
-    PM_QOS_PID=$!
+	PM_QOS_PID=$!
+
+	# THP Disable
+	THP_ENABLED=/sys/kernel/mm/transparent_hugepage/enabled
+	saved_thp_enable="$(awk '{start=index($0,"[");end=index($0,"]");print substr($0,start+1,end-start-1)}' $THP_ENABLED)"
+	echo never > $THP_ENABLED
+
+	# Disable write backs for old data altogether
+	saved_dwc_value="$(cat /proc/sys/vm/dirty_writeback_centisecs)"
+	echo 0 > /proc/sys/vm/dirty_writeback_centisecs
+
+	# Set dirty data to expire at the maximum time limit for a given benchmark
+	saved_dec_value="$(cat /proc/sys/vm/dirty_expire_centisecs)"
+	echo $(($mlimit / 10)) > /proc/sys/vm/dirty_expire_centisecs
+
+	saved_dirty_bg_ratio="$(cat /proc/sys/vm/dirty_background_ratio)"
+	echo 90 > /proc/sys/vm/dirty_background_ratio
+
+	saved_dirty_ratio="$(cat /proc/sys/vm/dirty_ratio)"
+	echo 90 > /proc/sys/vm/dirty_ratio
 fi
 
 NSECS_OVERHEAD=$($VARROOT/bin/tattle -O)
@@ -156,29 +205,39 @@ fi
 printf "!CPU_NAME:            %45s\n" "$p_type"
 
 if [ L"`uname -s`" = L"Linux" ]; then
-    lscpu | sed 's/(s)/s/g' | sed -r 's/: +/:/g' | sed 's/, /,/g' | sed 's/ /_/g' | sed 's/:/ /g' | sed 's/Architecture/Processor/g' | sed -r 's/^CPUs/#CPUs/g' | sed 's/CPU_sockets/Sockets/g' | awk '{name=$1; val=$2; printf("!%-20s %45s\n", name ":", val);}'
+	printf "!Boot cmd line:       %45s\n" "$(cat /proc/cmdline)"
 
-    dmidecode --type 17 2> /dev/null | awk -f $VARROOT/bin/mem.awk | awk '{printf("!%-40s %25s\n", "Memory:", $1 "," $2 "," $3)}'
+	lscpu | sed 's/(s)/s/g' | sed -r 's/: +/:/g' | sed 's/, /,/g' | sed 's/ /_/g' | sed 's/:/ /g' | sed 's/Architecture/Processor/g' | sed -r 's/^CPUs/#CPUs/g' | sed 's/CPU_sockets/Sockets/g' | awk '{name=$1; val=$2; printf("!%-20s %45s\n", name ":", val);}'
 
-    sysctl -A 2> /dev/null | grep sched | grep -v sched_domain | awk '{printf("!%-40s %25s\n", $1 ":", $3)}'
+	dmidecode --type 17 2> /dev/null | awk -f $VARROOT/bin/mem.awk | awk '{printf("!%-40s %25s\n", "Memory:", $1 "," $2 "," $3)}'
 
-    numactl --hardware | awk -f $VARROOT/bin/numactl.awk
+	sysctl -A 2> /dev/null | grep sched | grep -v sched_domain | awk '{printf("!%-40s %25s\n", $1 ":", $3)}'
+
+	printf "!Trans. Huge Page:    %45s\n" "$(cat /sys/kernel/mm/transparent_hugepage/enabled)"
+	printf "!THP Defrag:          %45s\n" "$(cat /sys/kernel/mm/transparent_hugepage/defrag)"
+	printf "!THP Debug COW:       %45s\n" "$(cat /sys/kernel/mm/transparent_hugepage/debug_cow)"
+
+	(cd /sys/kernel/mm/transparent_hugepage/khugepaged; for i in *; do printf "!THP khpd.%-30s %26s\n" "$i:" "$(awk '{$1=$1;print}' $i)"; done) 2> /dev/null
+
+	(cd /proc/sys/vm; for i in *; do printf "!vm.%-30s %32s\n" "$i:" "$(awk '{$1=$1;print}' $i)"; done) 2> /dev/null
+
+	numactl --hardware | awk -f $VARROOT/bin/numactl.awk
 else
-    if [ -f /usr/sbin/psrinfo ]; then
-    	p_count=`psrinfo|wc -l`
-    	p_mhz=`psrinfo -v | awk '/operates/{print $6 "MHz"; exit }'`
-    elif [ -f /proc/cpuinfo ]; then
-    	p_count=`egrep processor /proc/cpuinfo | wc -l`
-    	p_mhz=`awk -F: '/cpu MHz/{printf("%5.0f00Mhz\n",$2/100); exit}' /proc/cpuinfo`
-    elif [ F"`uname -s`" = F"FreeBSD" ] ; then
-    	p_count=`sysctl -n hw.ncpu`
-    	p_mhz=`sysctl -n hw.clockrate`
-    else
-    	p_count='Unknown'
-    	p_mhz='Unknown'
-    fi  
-    printf "!#CPUs:        %30s\n" $p_count
-    printf "!CPU_MHz:      %30s\n" $p_mhz
+	if [ -f /usr/sbin/psrinfo ]; then
+		p_count=`psrinfo|wc -l`
+		p_mhz=`psrinfo -v | awk '/operates/{print $6 "MHz"; exit }'`
+	elif [ -f /proc/cpuinfo ]; then
+		p_count=`egrep processor /proc/cpuinfo | wc -l`
+		p_mhz=`awk -F: '/cpu MHz/{printf("%5.0f00Mhz\n",$2/100); exit}' /proc/cpuinfo`
+	elif [ F"`uname -s`" = F"FreeBSD" ] ; then
+		p_count=`sysctl -n hw.ncpu`
+		p_mhz=`sysctl -n hw.clockrate`
+	else
+		p_count='Unknown'
+		p_mhz='Unknown'
+	fi  
+	printf "!#CPUs:        %30s\n" $p_count
+	printf "!CPU_MHz:      %30s\n" $p_mhz
 fi
 
 while read A B C
@@ -226,10 +285,5 @@ do
 	echo "#"
 	$VARROOT/bin/$A -? | awk '{$1=$1;print}' | awk '/^[\[-]/ {sub(/^/, "    ")}{sub(/^/, "# ");print}'
 done < $VARROOT/suites/$suite.txt
-
-# Clean up background processes
-if [ -n "$PM_QOS_PID" ]; then
-	kill -INT $PM_QOS_PID
-fi
 
 exit 0
